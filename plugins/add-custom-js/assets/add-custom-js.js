16cc68c77acf2b844cc08828e640aae334edd70e
tripledoublev <tripledoublev@users.noreply.github.com>
2024-04-25 20:21:53 +0000

    deploy: bfba196f090c60f3cc2cb40e7e08497bb8b1818a

---
+++ plugins/add-custom-js/assets/add-custom-js.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.src = 'https://analytics.hypha.coop/js/script.js';
+script.defer = true;
+script.setAttribute('data-domain', 'handbook.hypha.coop');
+document.head.appendChild(script);
---
+++ plugins/add-custom-js/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    website: {
+      assets: "./assets",
+      js: [
+        "add-custom-js.js"
+      ]
+    }
+  };
+  
